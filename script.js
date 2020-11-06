70d7113aa8f28c2be3525f620982f37c8893fe34
Alice Piar <adpir30@gmail.com>
2020-11-06 02:33:58 -0600

    New changes

--- script.js
+++ script.js
@@ -1,76 +1,91 @@
-var previouscity = JSON.parse(localStorage.getItem("weatherlist"))|| []
+let previouscity=JSON.parse(localStorage.getItem("#Weatherlist")) || []
 $(document).ready(function(){
     $("#submit").click(function(){
         let city =$("#city").val();
         if(city != '') {
-            Currentshow (city)
-            FivedaysShow (city)
             console.log(city)
-            previouscity.push(city)
-            localStorage.setItem("weatherlist",JSON.stringify(previouscity))
-            $(".list-group").prepend(`<li class="list-group-item">${city}</li>`)
-        }else {
-            $("#error").html('field cannot be empty');
-        }
-    });
-});
-function uvishow(lon,lat){
-    $.ajax({
-        url:"https://api.openweathermap.org/data/2.5/uvi?lat=" +lat + "&lon=" + lon + 
-        "&APPID=b5ffc1b446f183868b291ba8f08a28ae",
-        method:"GET",
-        dataType:"json", 
-        success: function(data){
-          console.log(data);
-          $("#uvi").append(`<h4 clas="bg-warning"> uviindex: ${data.value}</h4>`)
+            // FiveDaysForecast()
+             previouscity.push(city)
+             localStorage.setItem("#Weatherlist",JSON.stringify(previouscity))
+             $(".list-group").prepend(`<li class="list-group-item">${city}</li>`);
+            $.ajax({
+                url:'https://api.openweathermap.org/data/2.5/weather?q=' + city + "&units=imperial" + 
+                "&APPID=b5ffc1b446f183868b291ba8f08a28ae",
+                method:"GET",
+                dataType:"json", 
+                success: function(data){
+                    console.log(data);
+                    $.ajax({
+                        url:'https://api.openweathermap.org/data/2.5/onecall?lat=' + data.coord.lat +'&lon='+ data.coord.lon + '&appid=b5ffc1b446f183868b291ba8f08a28ae',
+                        method:"GET",
+                        dataType:"json", 
+                        success:function(FiveDays){
+                        let widget =show(data,FiveDays);
+                        $("#show").html(widget);
+                        $("#city").val('');
 
-        }       
-})}
 
-function FivedaysShow(city){
-    $.ajax({
-        url:'https://api.openweathermap.org/data/2.5/forecast?q=' + city + "&units=imperial" + 
-        "&APPID=b5ffc1b446f183868b291ba8f08a28ae",
-        method:"GET",
-        dataType:"json", 
-        success: function(data){
-          console.log(data);
-        var data= data.list
-        let widget =""
-        for (let i=0; i< data.length; i=i +8) {
-            widget+=`<div class="card bg-primary"<h1> ${data[i].dt_txt.split(" ")[0]} </h1> <h2> Weather :${data[i].weather[0].description}</h2>
-            <h2> Temp: ${data[i].main.temp}</h2>
-            <h4>Wind speed: ${data[i].wind.speed}
-            <img src="https://openweathermap.org/img/wn/${data[i].weather[0].icon}@2x.png"/></div>`
+
+                        }
+                //  let FiveDays=FiveDaysForecast(data);
+                //  console.log(FiveDays);
+                 
+                  
+        });        
+      
+    //  success: function(FiveDays){
+    // //     return(FiveDays);
+
+    //                 }       
+                
+        }});
+                
            
+            
+            
 
-        }
-        $(".Weather-list").html(widget)
-       
-         } 
-      });
+
+function show(data, FiveDays){
+    console.log(FiveDays);
+    return "<h2>Name:" +data.name +"</h2>" +
+    "<h2>Weather:" +data.weather[0].main +"</h2>"+
+    "<h2>Description: "+data.weather[0].description +"</h2>"+
+    "<h2>Temperature:" +data.main.temp +"</h2>" +
+    "<h2>Humidity:" +data.main.humidity +"</h2>" +
+    "<h2>Wind-Speed:" +data.wind.speed +"</h2>"+
+    "<h2>uvi-index:" +FiveDays.current.uvi +"</h2>"+
+    "<h2>lon:" +data.coord.lon +"</h2>"
+
 }
-    function Currentshow(city){
-        $.ajax({
-            url:'https://api.openweathermap.org/data/2.5/weather?q=' + city + "&units=imperial" + 
-            "&APPID=b5ffc1b446f183868b291ba8f08a28ae",
-            method:"GET",
-            dataType:"json", 
-            success: function(data){
-              console.log(data);
-              var lat=data.coord.lat
-              var lon=data.coord.lon
-              uvishow(lon,lat)
-           let widget=`<div class="bg-primary">
-           <h1> ${data.name} </h1> <h2> Weather :${data.weather[0].description}</h2>
-           <h2> Temp: ${data.main.temp}</h2>
-           <h4>Wind speed: ${data.wind.speed}
-           <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png"/>
-           </div>`
-           $("#show").html(widget);
-           $("#city").val('');
-           
-             } 
-          });
+  
 
-    }
+
+
+
+//  function FiveDaysForecast(data){
+//     //  $.ajax({
+//     // url:'https://api.openweathermap.org/data/2.5/onecall?lat=' + data.coord.lat +'&lon='+ data.coord.lon + '&appid=b5ffc1b446f183868b291ba8f08a28ae',
+//     // method:"GET",
+//     // dataType:"json", 
+//     // success: function(FiveDays){
+//     //     return(FiveDays);
+        
+        
+//         // let Weatherlist =show(data,FiveDays);
+       
+// //         for ( let i=0; i< data.length;i=i +8){
+// //       let data= data[i];
+// //       let data=data.coord.lat
+// //       let data= data.coord.lon
+// //  }
+
+        
+
+//       }}
+    
+
+}});
+
+});
+   
+     

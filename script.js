1be2ec73b0f884a57671db7ffb5a56c8b8078046
Alice Piar <adpir30@gmail.com>
2020-11-07 17:40:10 -0600

    new updates

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-let previouscity = JSON.parse(localStorage.getItem("#Weatherlist")) || []
-// let forecast=JSON.parse(localStorage.getItem("#Dailyforecast")) || []
+let previouscity = JSON.parse(localStorage.getItem("#Weatherlist"))|| []
 $(document).ready(function(){
     $("#submit").click(function(){
         let city =$("#city").val();
@@ -8,9 +7,12 @@ $(document).ready(function(){
             Weather(city)
             FiveDaysForecast(city)
              previouscity.push(city)
-             localStorage.setItem("weatherlist",JSON.stringify(previouscity))           
-            //  forecast.push(FiveDays);
+             localStorage.setItem("#Weatherlist".JSON.stringify(previouscity));
              $(".list-group").prepend(`<li class="list-group-item">${city}</li>`);
+             //History item saved//
+            //  $(".list-group-item").on("click",function(){
+            //     console.log()
+            //  })
         }else {
             $(".msg").html('Error-Please enter a City');
         }
@@ -36,8 +38,7 @@ $(document).ready(function(){
 
 
 
-        //                 // //  for(let i=0; i <data.lenght; i=i+8);
-        //                 // //   
+
 
 
 
@@ -56,7 +57,7 @@ $(document).ready(function(){
 
          }
 
-
+       
          function FiveDaysForecast(city){
             $.ajax({
              url:'https://api.openweathermap.org/data/2.5/forecast?q=' + city + "&units=imperial" +
@@ -65,19 +66,46 @@ $(document).ready(function(){
             dataType:"json",
             success:function(data){
             console.log(data);
-            for (let i=0; i< data.length; i=i +8) {
-                let data =data[i];
-                //   widget+=`<div class="card bg-primary"<h1> ${data[i].dt_txt.split(" ")[0]} </h1>  <h2> Weather :${data[i].weather[0].description}</h2>
-                // // <img src="https://openweathermap.org/img/wn/${data[i].weather[0].icon}@2x.png"/></div>`
-               
-            }
+       
+            $(".info-icon").html("<img src='http://openweathermap.org/img/w/" + data.list[0].weather.icon + ".png>");
+            $("#temp").html(data.list[0].main.temp);
+            $("#date").html(data.list[0].dt_txt);
+            $("#date").html(data.list[0].dt_txt);
+            // for (let i=0; i< data.length; i=i +8) {
+            //     let FiveDays=show(data);
+            //     $("#show").html(FiveDays);
+                  
+              
+                   
             
-            //  $(".weatherWidget").html(widget)
-            }
 
-            });
+           
+            // // // // //     <img src="https://openweathermap.org/img/wn/${data[i].weather[0].icon}@2x.png"/></div>`
+
+            // // }
+
+            // //  $("#weatherInfo").html(container2)
+            // }
+
+            }});
         }
-//
+    //      function GenerateForecast(data){
+    //          let html =`
+    //           <h3 class="temp">${data.main.temp}</h3>
+    //         <h3 class="status">${data.main.list.map(item =>item).join
+    //     ('')}</h3>
+    // //   <div id="weatherInfo">
+    // //   <div class="info-icon"></div>
+    // //    <p>Date</p>
+    // //    <p>Temp</p>
+    // //    <p>Huminity</p>
+    // //    <div id="icon2"></div>
+    // //     </div>
+    // //     `; 
+      
+    //         }
+//    $(".weatherInfo").html()
+    //     }
         //     $.ajax({
         //         url:'https://api.openweathermap.org/data/2.5/weather?q=' + city + "&units=imperial" +
         //         "&APPID=b5ffc1b446f183868b291ba8f08a28ae",
@@ -114,7 +142,7 @@ $(document).ready(function(){
 
         // //                 // }
         // //         //  let FiveDays=FiveDaysForecast(data);
-        // //         //  console.log(FiveDays);
+        // //
 
 
 
@@ -125,12 +153,16 @@ $(document).ready(function(){
 
 function show(data){
     console.log(data);
-     return "<h2>Name:" +data.name +"</h2>" +
+     return "<h2>Name:" +data.name +"</h2>"+
     "<h2>Weather:" +data.weather[0].main +"</h2>"+
     "<h2>Description: "+data.weather[0].description +"</h2>"+
     "<h2>Temperature:" +data.main.temp +"</h2>" +
     "<h2>Humidity:" +data.main.humidity +"</h2>" +
     "<h2>Wind-Speed:" +data.wind.speed +"</h2>"
+  
+
+
+
 
 
 

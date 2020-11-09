b6ae531398005d9228740d478c391bb16d941ee2
Alice Piar <adpir30@gmail.com>
2020-11-09 02:48:17 -0600

    final updates

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
  let city ="";
+
 $(document).ready(function() {
     let previouscity = JSON.parse(localStorage.getItem("#Weatherlist")) || []
     let place = '<input type="text" value=" ' + previouscity[0] + '" name="city" id="city" class="form-control">'
@@ -8,7 +9,7 @@ $(document).ready(function() {
          city = $("#city").val();
         if (city != '') {
             console.log(city)
-            Weather(city)
+           Weather(city)
            previouscity.push(city)
            console.log(previouscity)
             localStorage.setItem("#Weatherlist", JSON.stringify(previouscity));
@@ -20,7 +21,7 @@ $(document).ready(function() {
     });
 
 
-
+ // Performing an AJAX request with the queryURL
     function Weather(city) {
         $.ajax({
             url: 'https://api.openweathermap.org/data/2.5/weather?q=' + city + "&units=imperial" +
@@ -32,7 +33,8 @@ $(document).ready(function() {
                 let widget = show(data);
                 $("#show").html(widget);
                 $("#city").val('');
-                $("#show").append("<img src='http://openweathermap.org/img/w/" + data.weather[0].icon + ".png' alt='Icon depicting current weather.'>");
+
+
                 FiveDaysForecast(data.coord.lat, data.coord.lon);
                 uvishow(data.coord.lat, data.coord.lon);
 
@@ -43,30 +45,30 @@ $(document).ready(function() {
     }
     function Citylist(CityNames){
         for (i = 0; i < CityNames.length; i++) {
-            let history= '<li class="list-group-item" onclick="Weather(' + CityNames[i] + ')"> ' + CityNames[i] + '</li>';
+            let history= '<li class="list-group-item" (' + CityNames[i] + ')"> ' + CityNames[i] + '</li>';
             $(".list-group").prepend(history);
-            
+
 
 
         }
 
     }
 
-     $(".list-group").on("click","li",function() { 
+     $(".list-group").on("click","li",function() {
          $("#uvi").empty();
          city =$(this).text();
          Weather(city);
          FiveDaysForecast();
      })
-    
+
     function KevinToFarhenheit(tempInKevin) {
-        return ((tempInKevin - 273.15) * 1.8) + 32 
-        
+        return ((tempInKevin - 273.15) * 1.8) + 32
+
 
 
 
     }
-
+ // Performing an AJAX request with the queryURL
     function FiveDaysForecast(lat, lon) {
         $.ajax({
             url: 'https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&appid=b5ffc1b446f183868b291ba8f08a28ae',
@@ -81,25 +83,25 @@ $(document).ready(function() {
                 let forecastRowDiv = $('<div class="row"></div>');
                 forecastPlaceholder.append(forecastRowDiv);
 
-
+             //Create a for loop
                 for (i = 0; i < 5; i++) {
-                    let styledDiv = $('<div class="col-sm-2" style="background-color:lightblue"></div>');
+                    let styledDiv = $('<div class="col-sm-2" </div>');
                     let IconWebsite = $("<img>").attr("src","https://openweathermap.org/img/w/" + data.daily[i].weather[0].icon + ".png");
                     let Date =data.daily[i].dt;
                     let dates = moment.unix(Date).format('LL');
 
-                    styledDiv.append("Temp:" + Math.floor(KevinToFarhenheit(data.daily[i].temp.day)) +"&deg;F" + "</p>");
+                    styledDiv.append("Temp: " + Math.floor(KevinToFarhenheit(data.daily[i].temp.day)) +"&deg;F" + "</p>");
                     styledDiv.append("Humidity:" + data.daily[i].humidity + "</p>");
-                    styledDiv.append(dates);
                     styledDiv.append(IconWebsite);
+                    styledDiv.append(dates);
                     forecastRowDiv.append(styledDiv);
 
                 }
 
-               
+
             }
         });
-        
+
     }
 
     function uvishow(lat, lon) {
@@ -108,10 +110,9 @@ $(document).ready(function() {
                 "&APPID=b5ffc1b446f183868b291ba8f08a28ae",
             method: "GET",
             dataType: "json",
-            success: function(data2) {
-                console.log(data2);
-                $("#uvi").append(`<h4 clas="bg-warning"> uviindex: ${data2.value}</h4>`)
-
+            success: function(uIndex) {
+                console.log(uIndex);
+                $("#uvi").append(`<h3 clas="bg-warning"> UV Index: ${uIndex.value}</h3>`)
             }
         })
     }
@@ -120,13 +121,11 @@ $(document).ready(function() {
 
     function show(data) {
         console.log(data);
-        return "<h2>Name:" + data.name + "</h2>" +
-            "<h2>Weather:" + data.weather[0].main + "</h2>" +
-            "<h2>Description: " + data.weather[0].description + "</h2>" +
-            "<h2>Temperature:" + data.main.temp + "</h2>" +
-            "<h2>Humidity:" + data.main.humidity + "</h2>" +
-            "<h2>Wind-Speed:" + data.wind.speed + "</h2>"
-
+        return "<h3>" + data.name + "<img src='http://openweathermap.org/img/w/" + data.weather[0].icon + ".png' alt='current weather icon.'>"+"</h3>"+
+            "<h3>Temperature:" + data.main.temp  +"&deg;F </h3>" +
+            "<h3>Humidity:" + data.main.humidity + "% </h3>" +
+            "<h3>Wind-Speed:" + data.wind.speed + " MPH </h3>"
+            
 
     }
 
